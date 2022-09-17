0e307d12a0a6ecd549fb2965fd5d59ffbc0191c7
RichardPhilips <richard.philips5551@gmail.com>
2022-09-17 21:24:03 +0200

    fix: styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
       <img
         src={`${process.env.PUBLIC_URL}/media/logo.png`}
         alt='Hydrolife Logo'/>
         <p>Coming soon...</p>
         <a href="tel:0217050306">021-705-0306</a>
-      </header>
     </div>
   );
 }
