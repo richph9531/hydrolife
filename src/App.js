a0e74f2ce65a440e528cfe4b34901c1e7fff603b
RichardPhilips <richard.philips5551@gmail.com>
2022-09-17 22:05:44 +0200

    fix: small logo

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img
-        src={`${process.env.PUBLIC_URL}/media/logo.png`}
-        alt='Hydrolife Logo'/>
-        <p>Coming soon...</p>
-        <a href="tel:0217050306">021-705-0306</a>
+        <img 
+          src={`${process.env.PUBLIC_URL}/media/logo-small.png`}
+          alt='Hydrolife Logo'/>
+      <p>Coming soon...</p>
+      <a href="tel:0217050306">021-705-0306</a>
     </div>
   );
 }
