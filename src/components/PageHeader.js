f18af798f000c3f7a7bb93f60708b92580d4ad55
Richard Philips <richard.philips@johnlewis.co.uk>
2022-09-20 09:46:11 +0200

    fix: remove webVitals and test before deploy

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -5,14 +5,14 @@ import classes from '../styles/PageHeader.module.css';
 const PageHeader = () => (
   <div className={classes.PageHeader}>
     <div className={classes.LogoContainer}>
-      <img
-        src={`${process.env.PUBLIC_URL}/media/Logo.png`}
+      <img 
+        src={`${process.env.PUBLIC_URL}/media/logo.png`}
         alt='Hydrolife Logo'/>
       </div>
     <div className={classes.MenuContainer}>
-      <img
+      {/* <img
         src={`${process.env.PUBLIC_URL}/media/menu.png`}
-        alt='Menu'/>
+        alt='Menu'/> */}
     </div>
   </div>
 );
