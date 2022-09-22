aaf536212620db9e8c2ff65e1e17ab09383d9c15
Richard Philips <richard.philips@johnlewis.co.uk>
2022-09-22 09:19:02 +0200

    fix: update test

--- src/components/tests/MeetTheTeam.test.js
+++ src/components/tests/MeetTheTeam.test.js
@@ -3,7 +3,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import MeetTheTeam from '../MeetTheTeam';
 
+import teamData from '../../data/meetTheTeam.json';
+
+
 test('Meet the Team renders correctly', () => {
-  const view = render(<MeetTheTeam />);
+  const view = render(<MeetTheTeam  data={teamData}/>);
   expect(view.baseElement).toMatchSnapshot();
 });
